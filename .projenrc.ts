3969a4643736566f28cfb63ff9c503281a4e9a49
cx-ruiaraujo <rui.araujo@checkmarx.com>
2024-04-10 16:45:41 +0100

    test xpto

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,45 +3,6 @@ import { Job } from 'projen/lib/github/workflows-model';
 import { ReleaseTrigger } from 'projen/lib/release';
 import { BundleKics } from './projenrc';
 
-const jobConfig = {
-  name: 'Trivy Scan',
-  runsOn: ['ubuntu-20.04'],
-  permissions: {}, // Placeholder for permissions
-  steps: [
-    {
-      name: 'Checkout code',
-      uses: 'actions/checkout@v4',
-    },
-    {
-      name: 'Run Trivy vulnerability scanner in repo mode',
-      uses: 'aquasecurity/trivy-action@master',
-      with: {
-        'scan-type': 'fs',
-        'ignore-unfixed': true,
-        'format': 'json',
-        'output': './trivy-results.json',
-        'severity': 'CRITICAL,HIGH,MEDIUM',
-        'exit-code': '1',
-      },
-    },
-    {
-      name: 'Inspect action report',
-      if: 'always()',
-      shell: 'bash',
-      run: 'cat ./trivy-results.json',
-    },
-    {
-      if: 'always()',
-      name: 'Upload artifact',
-      uses: 'actions/upload-artifact@v2',
-      with: {
-        name: 'trivy code report',
-        path: './trivy-results.json',
-      },
-    },
-  ],
-};
-
 const project = new JsiiProject({
   author: 'Checkmarx',
   authorAddress: 'kics@checkmarx.com',
@@ -124,13 +85,55 @@ if (releaseWorkflow != null) {
   }
 }
 
-const secChecksWorkflow = project.github?.workflows.find((wf) => wf.name === 'sec-checks');
-console.log(project.github?.tryFindWorkflow('sec-checks'));
+const jobConfig = {
+  name: 'Trivy Scan',
+  runsOn: ['ubuntu-20.04'],
+  permissions: {}, // Placeholder for permissions
+  steps: [
+    {
+      name: 'Checkout code',
+      uses: 'actions/checkout@v4',
+    },
+    {
+      name: 'Run Trivy vulnerability scanner in repo mode',
+      uses: 'aquasecurity/trivy-action@master',
+      with: {
+        'scan-type': 'fs',
+        'ignore-unfixed': true,
+        'format': 'json',
+        'output': './trivy-results.json',
+        'severity': 'CRITICAL,HIGH,MEDIUM',
+        'exit-code': '1',
+      },
+    },
+    {
+      name: 'Inspect action report',
+      if: 'always()',
+      shell: 'bash',
+      run: 'cat ./trivy-results.json',
+    },
+    {
+      if: 'always()',
+      name: 'Upload artifact',
+      uses: 'actions/upload-artifact@v2',
+      with: {
+        name: 'trivy code report',
+        path: './trivy-results.json',
+      },
+    },
+  ],
+};
+
+const secChecksWorkflow = project.github?.tryFindWorkflow('sec-checks');
 if (secChecksWorkflow != null) {
-  secChecksWorkflow.addJob('secchecks', { ...jobConfig }); // for job config: https://github.com/projen/projen/blob/65b4194c163f47ba4842981b0c92dbe516be787b/src/github/workflows-model.ts#L6
+  secChecksWorkflow.addJob('secchecks', { ...jobConfig });
 } else {
   const wfXpto = project.github?.addWorkflow('sec-checks');
   wfXpto?.addJob('secchecks', { ...jobConfig });
+  wfXpto?.on({
+    push: { branches: ['main'] },
+    pullRequest: {},
+  });
 }
 
 new BundleKics(project);
