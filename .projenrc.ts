937bea6850f68c916f416e34472299a19c820208
cx-ruiaraujo <rui.araujo@checkmarx.com>
2024-04-10 15:35:18 +0100

    test 3

--- .projenrc.ts
+++ .projenrc.ts
@@ -38,9 +38,59 @@ project.tsconfig?.addInclude('projenrc/**/*.ts');
 project.gitignore.exclude('bin');
 project.gitignore.exclude('assets');
 
-const securityChecksJob = {
+// Super hacky way to add a step to a workflow that projen itself generates
+const buildWorkflow = project.github?. tryFindWorkflow('build');
+
+if (buildWorkflow != null) {
+  const buildJob = buildWorkflow.getJob('build');
+  if (isJob(buildJob)) {
+    buildWorkflow.updateJob('build', {
+      ...buildJob,
+      steps: [
+        {
+          uses: 'actions/setup-go@v5',
+          with: { 'go-version': '1.22.x' },
+        },
+        { run: 'go install github.com/goreleaser/goreleaser@latest' },
+        {
+          name: 'Add goreleaser to PATH',
+          run: 'echo "PATH=$(go env GOPATH)/bin:$PATH" >> $GITHUB_ENV',
+        },
+        ...(buildJob.steps as any)(),
+      ],
+    });
+  }
+}
+
+// Super hacky way to add a step to a workflow that projen itself generates
+const releaseWorkflow = project.github?. tryFindWorkflow('release');
+
+if (releaseWorkflow != null) {
+  const releaseJob = releaseWorkflow.getJob('release');
+  if (isJob(releaseJob)) {
+    releaseWorkflow.updateJob('release', {
+      ...releaseJob,
+      steps: [
+        {
+          uses: 'actions/setup-go@v5',
+          with: { 'go-version': '1.22.x' },
+        },
+        { run: 'go install github.com/goreleaser/goreleaser@latest' },
+        {
+          name: 'Add goreleaser to PATH',
+          run: 'echo "PATH=$(go env GOPATH)/bin:$PATH" >> $GITHUB_ENV',
+        },
+        ...releaseJob.steps,
+      ],
+    });
+  }
+}
+
+
+const jobConfig = {
   name: 'Trivy Scan',
-  runsOn: 'ubuntu-20.04',
+  runsOn: ['ubuntu-20.04'],
+  permissions: {}, // Placeholder for permissions
   steps: [
     {
       name: 'Checkout code',
@@ -76,53 +126,9 @@ const securityChecksJob = {
   ],
 };
 
-// Super hacky way to add a step to a workflow that projen itself generates
-const buildWorkflow = project.github?. tryFindWorkflow('build');
-
-if (buildWorkflow != null) {
-  const buildJob = buildWorkflow.getJob('build');
-  if (isJob(buildJob)) {
-    buildWorkflow.updateJob('build', {
-      ...buildJob,
-      steps: [
-        {
-          uses: 'actions/setup-go@v5',
-          with: { 'go-version': '1.22.x' },
-        },
-        { run: 'go install github.com/goreleaser/goreleaser@latest' },
-        {
-          name: 'Add goreleaser to PATH',
-          run: 'echo "PATH=$(go env GOPATH)/bin:$PATH" >> $GITHUB_ENV',
-        },
-        securityChecksJob,
-        ...(buildJob.steps as any)(),
-      ],
-    });
-  }
-}
-
-// Super hacky way to add a step to a workflow that projen itself generates
-const releaseWorkflow = project.github?. tryFindWorkflow('release');
-
-if (releaseWorkflow != null) {
-  const releaseJob = releaseWorkflow.getJob('release');
-  if (isJob(releaseJob)) {
-    releaseWorkflow.updateJob('release', {
-      ...releaseJob,
-      steps: [
-        {
-          uses: 'actions/setup-go@v5',
-          with: { 'go-version': '1.22.x' },
-        },
-        { run: 'go install github.com/goreleaser/goreleaser@latest' },
-        {
-          name: 'Add goreleaser to PATH',
-          run: 'echo "PATH=$(go env GOPATH)/bin:$PATH" >> $GITHUB_ENV',
-        },
-        ...releaseJob.steps,
-      ],
-    });
-  }
+const secChecksWorkflow = project.github?.workflows.find((wf) => wf.name === 'sec-checks');
+if (secChecksWorkflow != null) {
+  secChecksWorkflow.addJob('secchecks', { ...jobConfig }); // for job config: https://github.com/projen/projen/blob/65b4194c163f47ba4842981b0c92dbe516be787b/src/github/workflows-model.ts#L6
 }
 
 new BundleKics(project);
